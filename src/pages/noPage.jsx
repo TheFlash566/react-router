c53edc6a27250d98a90aed6bc5f84a5d50fd824a
TheFlash566 <mikebabs566@gmail.com>
2022-12-28 05:20:27 -0800

    updated

--- src/pages/noPage.jsx
+++ src/pages/noPage.jsx
@@ -1,6 +1,6 @@
 const NoPage = () => {
     return (
-      <div className="no-page" style={{padding: '15em'}}>
+      <div className="no-page" style={{padding: '14em', lineHeight: 2,}}>
           <h1 style={{textAlign: 'center',  color: 'red'}}>404 ERROR</h1>
           <h1 style={{textAlign: 'center', color: 'red'}}>Sorry Page Not Found!</h1>
       </div>
